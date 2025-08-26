e5606f1d5d5fce15889e012737cdc44df91c5c22
Fede <fsolariv@fi.uba.ar>
2025-08-26 15:17:55 -0300

    Solo fecha harcodeada

--- server/common/server.py
+++ server/common/server.py
@@ -59,9 +59,9 @@ class Server:
 
             bet_data = self.parse_bet_message(msg)
             if bet_data:
-                # bet = utils.Bet("1",  bet_data["first_name"],  bet_data["last_name"],  
-                # bet_data["document"],  bet_data["birthdate"],  bet_data["number"])
-                bet = utils.Bet("1", "fede", "solari", "123", "2020-10-22", "1234")
+                bet = utils.Bet("1",  bet_data["first_name"],  bet_data["last_name"],  
+                bet_data["document"],  "2020-10-22",  bet_data["number"])
+                # bet = utils.Bet("1", "fede", "solari", "123", "2020-10-22", "1234")
                 
                 logging.info(f'action: apuesta_almacenada | result: success | dni: {bet_data["document"]} | numero: {bet_data["number"]}')
 
@@ -155,15 +155,16 @@ class Server:
             if len(parts) != 5:
                 raise ValueError("Mensaje con cantidad de campos incorrecta")
 
-            first_name, last_name, document, birthdate, numbers = parts
-            # number = int(number_str)  # convertir el número a entero
+            first_name, last_name, document, birthdate, number_str = parts
+            # birthdate = datetime.date.fromisoformat(birthdate_str)
+            number = int(number_str) 
 
             return {
                 "first_name": first_name,
                 "last_name": last_name,
                 "document": document,
                 "birthdate": birthdate,
-                "number": numbers
+                "number": number
             }
         except Exception as e:
             logging.error(f"action: parse_bet | result: fail | error: {e}")
